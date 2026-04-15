e6d4e48b66ce85395a3bab6a004363cd44d8a54d
sixMenthes <sixmenthes@gmail.com>
2026-04-16 01:35:44 +0200

    add changes to encoder and FSQ

---
+++ scripts/encoder.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from foundation_model.BEATs import BEATs, BEATsConfig
+
+def load_beats(beats_ckpt):
+    # load the pre-trained checkpoints
+    checkpoint = torch.load(beats_ckpt, map_location="cpu")
+
+    cfg = BEATsConfig(checkpoint['cfg'])
+    BEATs_model = BEATs(cfg)
+    BEATs_model.load_state_dict(checkpoint['model'])
+    BEATs_model.eval()
+
+    for p in BEATs_model.parameters():
+        p.requires_grad_(False)
+
+    return BEATs_model
+
+class Encoder(nn.Module):
+    def __init__(self, beats_ckpt, levels):
+        super().__init__()
+        self.backbone = load_beats(beats_ckpt)
+        self.proj = nn.Linear(768, len(levels))
+        self.fsq = FSQ(levels)
--- scripts/ifsq.py
+++ scripts/ifsq.py
@@ -1,26 +1,17 @@
 import torch
 
-def iFSQ(z, levels):
-    '''
-    z: visual features map (B*H*W, D)
-    levels: list or tensor defining levels per dim (L)
-    '''
-    # 1. Bound input to [-1, 1]
-    z = 2 * torch.nn.Sigmoid(1.6 * z) - 1
 
-    # 2. Scale to the grid defined by levels
-    half_width = (levels - 1) / 2
-    z_scaled = z * half_width
+def round_ste(z):
+    zhat = torch.round(z)
+    return z + (zhat - z).detach()
+    
 
-    # 3. Quantization with Straight-Through Estimator
-    z_rounded = torch.round(z_scaled)
-    z_hat = z_rounded - z_scaled.detach() + z_scaled
+class FSQ:
+    def __init__(self, levels: list[int]):
+        self._levels = torch.tensor(levels)
+        self._basis = torch.cat([torch.tensor([1]), torch.cumprod(self._levels[:-1], dim=0)])
+        codebook_size = torch.prod(self._levels)
 
-    # 4. Compute indices for AR 
-    # basis: [L^(d-1), ..., L^0]    
+    def bound(self, z):
 
-    z_ind = z_rounded + half_width
-    basis = compute_basis(levels)
-    indices = torch.sum(z_ind * basis, dim=-1).long()
 
-    return indices 
